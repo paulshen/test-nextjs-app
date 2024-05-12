0b68fa27c55ee35e69fc5bdea7f0f01421ee6eeb
Paul Shen <paulshen0@gmail.com>
2024-05-12 15:40:44 -0700

    c headers

--- src/app/c/page.tsx
+++ src/app/c/page.tsx
@@ -1,6 +1,8 @@
+import { headers } from "next/headers";
 import Link from "next/link";
 
 export default async function PageC() {
+  const h = headers();
   await new Promise((resolve) => setTimeout(resolve, 300));
   const value = Math.random();
   return (
